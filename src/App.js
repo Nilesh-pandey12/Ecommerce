3724db2c1e46a2637290d0e1267d8ecb7e1bc6e8
Nilesh-pandey12 <95920342+Nilesh-pandey12@users.noreply.github.com>
2022-08-11 14:59:02 +0530

    some link changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import Landing from './Components/Routes/Landing'
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import AllCategory from './Components/Category/AllCategory';
 function App() {
   return (
     <>
       <Landing/>
-      {/* <Header/>
-      <AllCategory/>
-      <Footer/> */}
     </>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import logo from './logo.png'
-import logo2 from './logo2.png'
 import { Link } from 'react-router-dom'
 const Header = () => {
     return (
@@ -11,9 +10,9 @@ const Header = () => {
                 <ul className='flex justify-end space-x-16 mx-9 pt-14'>
                     <Link to='/'><li className='hover:bg-teal-900 hover:rounded-xl hover:text-white hover:px-2 hover:py-1'>Home</li></Link>
                     <Link to='/Category/AllCategory'><li className='hover:bg-teal-900 hover:rounded-xl hover:text-white hover:px-2 hover:py-1'>Category</li></Link>
-                    <li>Promo</li>
-                    <li>About</li>
-                    <li className='border-2 border-black h-10 w-28 text-center rounded-2xl p-1'>Contact us</li>
+                    <Link to='/'><li className='hover:bg-teal-900 hover:rounded-xl hover:text-white hover:px-2 hover:py-1 '>Promo</li></Link>
+                    <Link to='/'><li className='hover:bg-teal-900 hover:rounded-xl hover:text-white hover:px-2 hover:py-1 '>About</li></Link>
+                    <Link to='/'><li className='hover:bg-teal-900 hover:rounded-xl hover:text-white hover:px-2 hover:py-1'>Contact us</li></Link>
                 </ul>
 
             </div>
